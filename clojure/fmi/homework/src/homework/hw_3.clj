a2dbfe98aa1eb9117811f995222ce389342f4e51
Ivailo Hristov <ivcho92@gmail.com>
2013-03-27 15:35:59 +0200

    Added first three functions.

--- clojure/fmi/homework/src/homework/hw_3.clj
+++ clojure/fmi/homework/src/homework/hw_3.clj
@@ -5,3 +5,32 @@
 (def reset-counter! #(reset! *counter* 0))
 (def count!         #(swap! *counter* inc))
 (def get-count      #(deref *counter*))
+
+;;; ==================================
+
+(def calls-data (atom {}))
+(def safe-inc (fnil inc 0))
+
+(defn add-metrics [f fkey]
+  (fn [& args]
+    (swap! calls-data #(update-in % [fkey args] safe-inc))
+    (apply f args)))
+
+(defn calls [fkey & args]
+  (or (get-in @calls-data [fkey args]) 0))
+
+;;; ==================================
+
+(def ^:dynamic *after* {})
+(def ^:dynamic *before* {})
+
+(defn add-events [f fkey]
+  (fn [& args]
+    (when-let [before (*before* fkey)]
+      (apply before args))
+    (let [result (apply f args)]
+      (when-let [after (*after* fkey)]
+        (apply after result args))
+      result)))
+
+;;; ==================================
