be2ba6bac25d10cc42b12e6b5689d2e1234c0ab6
Ivailo Hristov <ivcho92@gmail.com>
2013-03-27 12:35:41 +0200

    Implemented counter.

--- clojure/fmi/homework/src/homework/hw_3.clj
+++ clojure/fmi/homework/src/homework/hw_3.clj
@@ -1,1 +1,7 @@
 (ns homework.hw_3)
+
+(def ^:dynamic *counter* (atom 0))
+
+(def reset-counter! #(reset! *counter* 0))
+(def count!         #(swap! *counter* inc))
+(def get-count      #(deref *counter*))
