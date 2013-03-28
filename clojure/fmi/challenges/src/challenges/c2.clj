70abb71288d7ecef775f9d676cb2e01a29ae4a60
Ivailo Hristov <ivcho92@gmail.com>
2013-03-28 12:28:03 +0200

    Challenge 2 improvements.

--- clojure/fmi/challenges/src/challenges/c2.clj
+++ clojure/fmi/challenges/src/challenges/c2.clj
@@ -1,11 +1,11 @@
 (ns challenges.c2)
 
-(def make-account #(atom 0))
 (def balance deref)
+(def make-account (partial atom 0))
 
 (defn deposit [account amount]
   (if (integer? amount)
-    (do (swap! account (partial + amount)) account)
+    (do (swap! account + amount) account)
     (throw (IllegalStateException. (str amount " is not an integer.")))))
 
 (defn withdraw [account amount]
