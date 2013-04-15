93fdace681be5ab77d10b63644d79107ccb92e7f
Ivailo Hristov <ivcho92@gmail.com>
2013-04-15 18:25:10 +0300

    Added 4 challange for clojure course in FMI.

--- clojure/fmi/challenges/src/challenges/c4.clj
+++ clojure/fmi/challenges/src/challenges/c4.clj
@@ -1,8 +1,18 @@
-;;; http://www.algorithmist.com/index.php/Coin_Change
 (ns challenges.c4)
 
-(def ^:const denominations [1 2 5 10 20 50 100])
+(def ^:private denoms [1 2 5 10 20 50 100])
+(def ^:private denoms-len (dec (count denoms)))
+(def ^:private count-change-helper
+    (memoize (fn [n m]
+       (cond
+        (= n 0) 1
+        (< n 0) 0
+        (and (< m 0) (>= n 1)) 0
+        :else (+ (count-change-helper n (dec m))
+                 (count-change-helper (- n (denoms m)) m))))))
 
-(defn count-change
-  [sum]
-  )
+(defn count-change [n] (count-change-helper n denoms-len))
+
+;; (prn (count-change 1)) ; 1
+;; (prn (count-change 4)) ; 3
+;; (prn (count-change 7)) ; 6
