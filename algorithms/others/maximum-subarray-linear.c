57091f99857e15065cfd7d48aeee38e7e1fb8c23
Ivailo Hristov <ivcho92@gmail.com>
2013-08-29 18:17:32 +0300

    Added maximum subarray linear solution.

---
+++ algorithms/others/maximum-subarray-linear.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "../helpers.h"
+#include "../testhelp.h"
+#include "maximum-subarray-linear.h"
+
+void tests() {
+  int A[8] = {4, 1, -32, 12, 17, -5, 1, 13};
+
+  subarr m = maximum_subarray(A, 0, 7);
+
+  test_cond("Check if maximum-subarray sum is 38.", m.sum == 38);
+  test_cond("Check if maximum-subarray left is 3.", m.left == 3);
+  test_cond("Check if maximum-subarray right is 7.", m.right == 7);
+
+  test_report();
+}
+
+int main() {
+  tests();
+
+  return 0;
+}
---
+++ algorithms/others/maximum-subarray-linear.h
@@ -0,0 +1,55 @@
+/*
+ * Maximum-subarray problem
+ * ------
+ *
+ * SEE: maximum-subarray.h
+ *
+ * This solution solves Maximum-subarray problem in linear time.
+ * Algorithm uses single for loop to iterate array and in each
+ * iteration keeps track of maximum subarray seen so far and maximum
+ * subarray ending on counter index. Current maximum subarray for the
+ * next interation will be grater ot the both from previous iteration.
+ *
+ * ASYMPTOTIC TIME:
+ *
+ *  T(n) = n
+ *
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+  int left;
+  int right;
+  int sum;
+} subarr;
+
+subarr maximum_subarray(int A[], int low, int high) {
+  if (low == high) {
+    subarr max_arr = {low, high, A[low]};
+    return max_arr;
+  }
+
+  unsigned i;
+
+  subarr curr_max_arr = {low, low, A[low]};
+  subarr right_max_arr = {low, low, A[low]};
+
+  for (i = 1; i <= high; i++) {
+    if (A[i] >= right_max_arr.sum && right_max_arr.sum < 0) {
+      right_max_arr.left = i;
+      right_max_arr.right = i;
+      right_max_arr.sum = A[i];
+    } else {
+      right_max_arr.right = i;
+      right_max_arr.sum += A[i];
+    }
+
+    if (right_max_arr.sum > curr_max_arr.sum) {
+      curr_max_arr = right_max_arr;
+    }
+  }
+
+  return curr_max_arr;
+}
