2d1e6484ab2b003bc5bc5a77a5927f841e90fb7d
ivo- <ivailo.nikolaev.hristov@gmail.com>
2013-12-14 23:48:00 +0200

    Clojure: Add reducers notes.

--- clojure/exploring/src/exploring/core/reducers.clj
+++ clojure/exploring/src/exploring/core/reducers.clj
@@ -1,55 +1,42 @@
+;;; Rich Hickey's posts:
+;;;
+;;; http://clojure.com/blog/2012/05/08/reducers-a-library-and-model-for-collection-processing.html
+;;; http://clojure.com/blog/2012/05/15/anatomy-of-reducer.html
 (ns exploring.core.reducers
+  "It is a new look at the fundamental functional operations map, filter, reduce
+   ... Traditional realizations work upon collections/sequences, transform them
+   and return new collections/sequences. Reducers take different approach by
+   working upon different abstraction - reducing functions.
+
+   Reducing function is just a binary function that can be passed to reduce.
+   Each of the map, filter, mapcat works by just producing and transforming
+   reducing function not knowing anything about the thing that will be reduced.
+   Reducing functions produced by map/filter/.. bound to collections are also
+   called reducibles.
+
+   Actual work is done by the reducer functions. There are two of them:
+     (reduce) -> respects the order
+     (fold)   -> parallel, doesn't respect the order
+
+   You wanna use reducers instead of core api when:
+     - you want speed
+     - you don't care about laziness"
   (:require [clojure.core.reducers :as r]))
 
-;;; This lib takes different approach at realizing the fundamental functions
-;;; map, filter and reduce. It makes the reasonable choice of treating reduce
-;;; singularly, and as a cornerstone of computing involving collections. It
-;;; defines:
-;;;
-;;; - "reducibles" things that can be reduced. They are basically functions
-;;;   holding collections.
-;;; - instead of transforming the collection, map and filter transform the
-;;;   reducer.
-;;; - actual work is done only when the reduce is called.
-;;;
 
-;;; Old functional api is more simpler but it takes more time to prepare lazy
-;;; sequences before after applying new map/filer/...
-(time (reduce + (map inc (map inc (map inc (range 10e5))))))
-(time (r/reduce + (r/map inc (r/map inc (r/map inc (range 10e3))))))
+;;; Equivalents
+(r/reduce + (r/map inc [1 2 3]))
+(reduce (fn [ret x] (+ ret (inc x))) (+) [1 2 3])
 
-;;; core.reducers/fold works similarly to map, but it is: automatically
-;;; parallizable, and implementing a “reduce/combine” model.
-;;; TODO:
-(time (r/fold + (r/map inc (r/map inc (r/map inc (range 10e5))))))
+;;; Mapping transformation of reducing function.
+(defn mapping [f]
+  (fn [f1]
+    (fn [result input]
+      (f1 result (f input)))))
 
-(defn benchmark [f N times]
-    (let [nums (vec (range N))
-          start (java.lang.System/currentTimeMillis)]
-      (dotimes [n times]
-        (f nums))
-      (- (java.lang.System/currentTimeMillis) start)))
+;;; Getting collection as a result.
+(into [] (r/filter even? (r/map inc [1 1 1 2])))
+(into #{} (r/filter even? (r/map inc [1 1 1 2]))) ;;= coll type as you wish
 
-(defn eager-map [& args] (doall (apply map args)))
-(defn eager-filter [& args] (doall (apply filter args)))
-
-(defn eager-test [nums] (eager-filter even? (eager-map inc nums)))
-(defn lazy-test [nums] (doall (filter even? (map inc nums))))
-(defn reducer-test [nums] (into [] (r/filter even? (r/map inc nums))))
-
-(println "Eager test:    " (benchmark eager-test 1000000 10) "ms")
-(println "Lazy test:     " (benchmark lazy-test 1000000 10) "ms")
-(println "Reducers test: " (benchmark reducer-test 1000000 10) "ms")
-
-(defn old-reduce [nums]
-  (reduce + (map inc (map inc (map inc nums)))))
-
-(defn new-reduce [nums]
-  (reduce + (r/map inc (r/map inc (r/map inc nums)))))
-
-(defn new-fold [nums]
-  (r/fold + (r/map inc (r/map inc (r/map inc nums)))))
-
-(println "Old reduce: " (benchmark old-reduce 10e5 10) "ms")
-(println "New reduce: " (benchmark new-reduce 10e5 10) "ms")
-(println "New fold:   " (benchmark new-fold 10e5 10) "ms")
+(r/reduce + (r/filter even? (r/map inc [1 1 1 2])))
+(r/fold + (r/filter even? (r/map inc [1 1 1 2])))
